94a3c14aa85173d63288f25c1073fc197ee92b69
Abdelwahab Kawafi <a.kawafi@bristol.ac.uk>
2021-12-02 18:38:45 +0000

    average precision working

--- trackpy_accuracy.py
+++ trackpy_accuracy.py
@@ -5,21 +5,22 @@ from torch._C import Value
 import trackpy as tp
 from src.deepcolloid import DeepColloid
 import math
+from sklearn import metrics
 
 
-def calc_iou_individual(center1, center2, diameter=10,):
+def calc_iou_individual(center1, center2, diameter,):
 	r = diameter / 2
 	c = math.sqrt((center1[0]-center2[0])**2 + (center1[1]-center2[1])**2 + (center1[2]-center2[2])**2)
 	area = math.pi*r**2
-
- 	q = 2* math.acos(math.radians(c/2*r))
-	overlap = (r**2)*(q - math.sin(q)) # area of overlap of circles with same r
-
-	print(c, r, overlap)
+	try:
+		q = 2* math.acos(math.radians(c/2*r))
+		overlap = (r**2)*(q - math.sin(q)) # area of overlap of circles with same r
+	except ValueError:
+		overlap = 0
 	iou = overlap / (area + area - overlap)
 	return iou
 
-def get_results(gt, pred, threshold, diameter,):
+def get_results(gt, pred, diameter, threshold,):
 
 	tp, fp, fn = 0, 0, 0
 
@@ -39,15 +40,15 @@ def get_results(gt, pred, threshold, diameter,):
 	ious = []
 	for ipb, pred_pos in enumerate(pred):
 		for igb, gt_pos in enumerate(gt):
-			print(pred_pos, gt_pos)
 			iou = calc_iou_individual(pred_pos, gt_pos, diameter)
 			if iou > threshold:
 				gt_idx_thresh.append(igb)
 				pred_idx_thresh.append(ipb)
 				ious.append(iou)
 
+	# sort by iou
 	args_desc = np.argsort(ious)[::-1]
-	print(args_desc)
+
 	if len(args_desc) == 0:
 		# No matches
 		tp = 0
@@ -59,7 +60,7 @@ def get_results(gt, pred, threshold, diameter,):
 		for idx in args_desc:
 			gt_idx = gt_idx_thresh[idx]
 			pr_idx = pred_idx_thresh[idx]
-			# If the boxes are unmatched, add them to matches
+			# If the boxes have not been matched, add them to matches
 			if (gt_idx not in gt_match_idx) and (pr_idx not in pred_match_idx):
 				gt_match_idx.append(gt_idx)
 				pred_match_idx.append(pr_idx)
@@ -69,35 +70,42 @@ def get_results(gt, pred, threshold, diameter,):
 
 	return tp, fp, fn
 
-def get_precision_recall(gt, pred, diameter=10, threshold=0.5,):
-	
-	tp, fp, fn = get_results(gt, pred, threshold, diameter)
-
-	try:
-		precision = tp/(tp + fp)
-	except ZeroDivisionError:
-		precision = 0.0
-	try:
-		recall = tp/(tp + fn)
-	except ZeroDivisionError:
-		recall = 0.0
-
-	precision = np.array(precision)
-	recall = np.array(recall)
-
-	return precision, recall
-
-def mAP(ground_truths, predictions, diameter=10, threshold=0.5,):
+def get_precision_recall(ground_truths, predictions, diameter, threshold):
 
 	precisions = []
 	recalls = []
 	for gt, pred in zip(ground_truths, predictions):
-		p, r = get_precision_recall(gt, pred, diameter, threshold)
-		precisions.append(p)
-		recalls.append(r)
-	precisions =  np.array(precisions)
-	recalls =  np.array(recalls)
+		tp, fp, fn = get_results(gt, pred, diameter, threshold,)
 
+		try:
+			prec = tp/(tp + fp)
+		except ZeroDivisionError:
+			prec = 0.0
+		try:
+			rec = tp/(tp + fn)
+		except ZeroDivisionError:
+			rec = 0.0
+		
+		precisions.append(prec)
+		recalls.append(rec)
+		
+	return np.mean(precisions), np.mean(recalls)
+
+def average_precision(ground_truths, predictions, diameter=10):
+
+	precisions = []
+	recalls = []
+	thresholds = np.linspace(0,1,11)
+	for thresh in thresholds:
+		print(thresh)
+		prec, rec = get_precision_recall(ground_truths, predictions, diameter, thresh,)
+		# precisions = precisions + prec
+		# recalls = recalls + rec
+		precisions.append(prec)
+		recalls.append(rec)
+	precisions = np.array(precisions)
+	recalls = np.array(recalls)
+	
 	prec_at_rec = []
 	for recall_level in np.linspace(0.0, 1.0, 11):
 		try:
@@ -108,24 +116,21 @@ def mAP(ground_truths, predictions, diameter=10, threshold=0.5,):
 		prec_at_rec.append(prec)
 	avg_prec = np.mean(prec_at_rec)
 
-	# precision recall
+	return precisions, recalls
 
-	return precisions, recalls, avg_prec
+def plot_pr_curve(precisions, recalls, label=None):
+	"""Simple plotting helper function"""
 
-def plot_pr_curve(precisions, recalls, category='Person', label=None, color=None, ax=None):
-    """Simple plotting helper function"""
 
-    if ax is None:
-        plt.figure(figsize=(10,8))
-        ax = plt.gca()
+	fig, ax = plt.subplots(figsize=(6,6))
+	ax.plot(recalls, precisions, label='Trackpy')
+	ax.set_xlabel('Recall')
+	ax.set_ylabel('Precision')
+	ax.legend(loc='center left')
+	ax.set_xlim(0,1.1)
+	ax.set_ylim(0,1.1)
 
-    ax.scatter(recalls, precisions, label=label, s=20)
-    ax.set_xlabel('recall')
-    ax.set_ylabel('precision')
-    ax.set_title('Precision-Recall')
-    ax.set_xlim([0.0,1.3])
-    ax.set_ylim([0.0,1.2])
-    return ax
+	return fig
 
 
 
@@ -146,19 +151,18 @@ if __name__ == '__main__':
 	dc = DeepColloid(dataset_path)
 
 	roiSize = (32,128,128)
-	threshold = 0.3
+	threshold = 0.9
 	weights_path =  'output/weights/unet.pt'
 	dataset_name = 'replicate'
 
 
 	multi_gt_pos = []
 	multi_pred_pos = []
-	for i in range(40, 46):
+	for i in range(40, 45):
 		canvas, gt_positions = dc.read_hdf5(dataset_name, i, return_positions=True)
 
-		f = tp.locate(canvas, 11)
+		f = tp.locate(canvas, 9)
 		f = [list(f[:]['z']), list(f[:]['y']), list(f[:]['x'])]
-
 		new = []
 		for i in range(0, len(f[0])):
 			new.append([ f[0][i], f[1][i], f[2][i] ])
@@ -166,11 +170,26 @@ if __name__ == '__main__':
 
 		multi_gt_pos.append(gt_positions)
 		multi_pred_pos.append(tp_predictions)
+	multi_gt_pos = np.array(multi_gt_pos)
+	multi_pred_pos = np.array(multi_pred_pos)
 
-	precisions, recalls, avg_prec = mAP(multi_gt_pos, multi_pred_pos)
+	print(multi_gt_pos[0].shape, multi_pred_pos[0].shape)
 
-	ax = plot_pr_curve(precisions, recalls)
-	plt.show()
+	precisions, recalls = average_precision(multi_gt_pos, multi_pred_pos)
+	
+	# recalls = np.array([1-r for r in recalls])
+
+	print(precisions, recalls)
+	# roc_auc = metrics.auc(precisions, recalls)
+	# display = metrics.RocCurveDisplay.from_predictions(fpr=recalls, tpr=precisions, roc_auc=roc_auc,
+	# 		estimator_name='Trackpy')
+	# display.plot()
+	display = metrics.PrecisionRecallDisplay(precision=precisions, recall=recalls, estimator_name='Trackpy').plot()
+
+	plt.savefig('output/trackpyroc.png')
+
+	# plt.show()
+	# fig = plot_pr_curve(precisions, recalls)
 
 
 
