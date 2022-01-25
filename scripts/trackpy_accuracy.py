6b0567f2ff4693e04e7aff61cde0fd8884444c71
Abdelwahab Kawafi <a.kawafi@bristol.ac.uk>
2022-01-25 11:24:12 +0000

    roc edits

--- scripts/trackpy_accuracy.py
+++ scripts/trackpy_accuracy.py
@@ -49,23 +49,7 @@ if __name__ == '__main__':
 	# dataset_path = '/mnt/storage/home/ak18001/scratch/Colloids'
 	dc = DeepColloid(dataset_path)
 
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	print(f'predict on {device}')
 
-	# model
-	model = UNet(in_channels=1,
-				out_channels=1,
-				n_blocks=4,
-				start_filters=32,
-				activation='relu',
-				normalization='batch',
-				conv_mode='same',
-				dim=3).to(device)
-
-	roiSize = (32,128,128)
-	threshold = 0.5
-	weights_path =  'output/weights/unet.pt'
-	dataset_name = 'third_run'  
 
 
 	canvas, gt_positions = dc.read_hdf5(dataset_name, 202, return_positions=True)
@@ -75,28 +59,50 @@ if __name__ == '__main__':
 	# recalls = np.array([1-r for r in recalls])
 	print(ap, precisions, recalls)
 
-	display = metrics.PrecisionRecallDisplay(precision=precisions, recall=recalls, estimator_name='Trackpy').plot()
-	# fig = plt.figure()
-	# ax = plt.gca()
-	# ax = plot_pr_curve(precisions, recalls, category='trackpy')
+	# display = metrics.PrecisionRecallDisplay(precision=precisions, recall=recalls, estimator_name='Trackpy').plot()
+	fig = plt.figure()
+	ax = plt.gca()
+	ax = plot_pr_curve(precisions, recalls, category='trackpy')
 	plt.savefig('output/roc_trackpy.png')
 
-	canvas, gt_positions = dc.read_hdf5(dataset_name, 202, return_positions=True)
-	label, tp_predictions = predict(canvas, model, device, weights_path, threshold=threshold, return_positions=True)
-	print(gt_positions.shape, tp_predictions.shape)
-	ap, precisions, recalls = dc.average_precision(gt_positions, tp_predictions, diameter=9)
-	# recalls = np.array([1-r for r in recalls])
-	print(ap, precisions, recalls)
 
-	display = metrics.PrecisionRecallDisplay(precision=precisions, recall=recalls, estimator_name='unet').plot()
-	fig = display.figure_
+
+
+
+
+	# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	# print(f'predict on {device}')
+	# # model
+	# model = UNet(in_channels=1,
+	# 			out_channels=1,
+	# 			n_blocks=4,
+	# 			start_filters=32,
+	# 			activation='relu',
+	# 			normalization='batch',
+	# 			conv_mode='same',
+	# 			dim=3).to(device)
+
+	# roiSize = (32,128,128)
+	# threshold = 0.5
+	# weights_path =  'output/weights/unet.pt'
+	# dataset_name = 'new_year'
+
+	# canvas, gt_positions = dc.read_hdf5(dataset_name, 202, return_positions=True)
+	# label, tp_predictions = predict(canvas, model, device, weights_path, threshold=threshold, return_positions=True)
+	# print(gt_positions.shape, tp_predictions.shape)
+	# ap, precisions, recalls = dc.average_precision(gt_positions, tp_predictions, diameter=9)
+	# recalls = np.array([1-r for r in recalls])
+	# print(ap, precisions, recalls)
+
+	# display = metrics.PrecisionRecallDisplay(precision=precisions, recall=recalls, estimator_name='unet').plot()
+	# fig = display.figure_
 
 	# precisions = np.array([1-p for p in precisions])
 	# fig = plt.figure()
 	# ax = plt.gca()
 	# thresholds = np.linspace(0.05,0.95,10)
 	# ax = plot_pr_curve(precisions, recalls, category='unet')
-	plt.savefig('output/roc_unet.png')
+	# plt.savefig('output/roc_unet.png')
 
 	# print(aps)
 	# print(np.mean(aps))
