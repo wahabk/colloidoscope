4bc6db9a30bb7d96a06e5c3310f3ad1fe66d0291
wahabk <akawafi3@gmail.com>
2022-07-14 10:58:34 +0100

    test viewer

--- tests/test.py
+++ tests/test.py
@@ -4,22 +4,22 @@ import matplotlib.pyplot as plt
 import napari
 
 if __name__ == '__main__':
-	dataset_path = '/home/ak18001/Data/HDD/Colloids'
-	# dataset_path = '/home/wahab/Data/HDD/Colloids'
+	# dataset_path = '/home/ak18001/Data/HDD/Colloids'
+	dataset_path = '/home/wahab/Data/HDD/Colloids'
 	# dataset_path = '/mnt/storage/home/ak18001/scratch/Colloids'
 	dc = DeepColloid(dataset_path)
 
-	dataset = 'replicate'
+	dataset = 'feb_psf_const'
 	n=1
 
 	nums = dc.get_hdf5_keys(dataset)
-
 	print(type(nums), nums)
-	scan, positions = dc.read_hdf5(dataset, n, return_positions=True)
-	label = dc.read_hdf5(dataset+'_labels', n)
 
-	print(scan.shape, label.shape)
+	data = dc.read_hdf5(dataset, n)
+	array, label, positions = data['image'], data['label'], data['positions']
 
-	# dc.view(scan, positions)
+	print(array.shape, label.shape, positions.shape)
+
+	dc.view(array, positions=positions, label=label)
 	# napari.run()
 
