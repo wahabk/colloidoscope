2767b5f9e797fa377910c9a499b276bdae64a885
wahabk <akawafi3@gmail.com>
2022-11-14 08:18:39 +0000

    setup fix for new branch

--- setup.py
+++ setup.py
@@ -29,9 +29,8 @@ setup(
                 'trackpy',
                 'neptune-client',
                 'ray[tune]',
-                # 'psf',
-                # 'perlin-numpy @ git+https://github.com/pvigier/perlin-numpy',
-                # 'monai @ git+https://github.com/wahabk/MONAI'
+                'perlin-numpy @ git+https://github.com/pvigier/perlin-numpy',
+                'monai @ git+https://github.com/wahabk/MONAI'
         ],
         keywords=['python', 'colloidoscope'],
 )
