b69c5bed212aa3942bcf848fd722f7b6fb135b01
wahabk <akawafi3@gmail.com>
2022-11-15 23:21:58 +0000

    trying  newaggy

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,14 +1,20 @@
 import numpy as np
 import colloidoscope
 import matplotlib.pyplot as plt
+import pandas as pd
 
 dc = colloidoscope.DeepColloid()
-array = dc.read_tif('examples/Data/emily.tiff')
-array = dc.crop3d(array, roiSize=(131,131,131))
+array = dc.read_tif('examples/Data/katherine.tiff')
+# array = dc.crop3d(array, roiSize=(128,128,128))
 print(array.shape)
-df, positions, label = dc.detect(array, diameter=13, weights_path='output/weights/attention_unet_202206.pt', debug=True, device="cpu")
+df, positions, label = dc.detect(array, diameter=13, 
+                                weights_path='output/weights/attention_unet_202206.pt', 
+                                debug=True, device="cpu",
+                                post_processing="tp")
 print(df)
 
 x,y = dc.get_gr(positions, 50,50)
 plt.plot(x,y,)
-plt.savefig("output/test/gr.png")
+plt.savefig(f"output/test/gr.png")
+
+dc.view(array,  label=label,  positions=positions)
