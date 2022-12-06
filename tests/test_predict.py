496a2d29610134a5091074ea58d59901908e1263
wahabk <akawafi3@gmail.com>
2022-12-06 17:00:01 +0000

    test predict on new weights

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -3,6 +3,7 @@ import colloidoscope
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as  np
+from colloidoscope.train_utils import exclude_borders
 
 dc = colloidoscope.DeepColloid()
 array = dc.read_tif('examples/Data/jamesdecon.tiff')
@@ -12,11 +13,15 @@ df, positions, label = dc.detect(array, diameter=15,
                                 weights_path='output/weights/attention_unet_202211.pt', 
                                 patch_overlap=(16,16,16),
                                 debug=True, device="cpu",
+                                roiSize=(100,100,100), label_size=(96,96,96),
                                 post_processing="log", batch_size=1)
 print(df)
 array = np.array(array, dtype="uint8")
 tp_pred, df =  dc.run_trackpy(array, diameter=15)
 print(df)
+
+tp_pred = exclude_borders(tp_pred, (256,256,256), pad = 15)
+positions = exclude_borders(positions, (256,256,256), pad = 15)
 
 x, y = dc.get_gr(tp_pred, 100, 100)
 plt.plot(x, y, label=f'tp n ={len(tp_pred)}', color='gray')
