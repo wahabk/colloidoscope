f06cb1b04fb8e31735637ecd384ea898a03c36e6
wahabk <akawafi3@gmail.com>
2023-01-02 11:50:40 +0000

    challenge final fix :)

---
+++ examples/Challenge/challenge_context_french.md
@@ -0,0 +1,22 @@
+
+# Colloïdes : Jusqu'à quelle taille peut-on aller ?
+
+<img src="https://i.postimg.cc/52FNft0w/fig-Website-Little2.png" alt="paddyfig1" width="800px"/>
+
+Le Royall Group est intéressé par l'utilisation de systèmes de nano-colloïdes pour comprendre la transition vitreuse, qui figure parmi les plus grands problèmes ouverts en physique des matériaux quotidiens.
+Les colloïdes ont des comportoments similaires aux atomes ou aux molécules, par exemple en formant des gaz (colloïdaux).
+Cependant, comme leur taille est de l'ordre du micron, ils peuvent être imagés avec des microscopes **3d** pour suivre leurs positions, et ainsi tester des théories sur la transition vitreuse (figure ci-dessus).
+Des colloïdes plus petits permettent des tests plus robustes des prédictions théoriques,
+car il est alors possible d'accéder à des états où l'on prédit une "nouvelle physique".
+À cet effet, nous avons utilisé une nouvelle "nanoscopie" super-résolution pour développer l'imagerie de nano-colloïdes, ce qui apporte de nouvelles observations sur la transition vitreuse.
+Cependant, si nous pouvions utiliser des particules encore plus petites, nous nous attendons à apprendre bien plus encore.
+Pour cette raison, nous souhaitons développer une méthode à base de machine learning for la détection de petits nano-colloïdes en 3d.
+
+# Contact (in english)
+
+C. Patrick “Paddy” Royall ESPCI Paris, paddy.royall@espci.fr
+
+Abdelwahab Kawafi, University of Bristol, a.kawafi@bristol.ac.uk
+
+Giulio Biroli, ENS Paris, giulio.biroli@ipht.fr
+
--- scripts/challenge/test_challenge.py
+++ scripts/challenge/test_challenge.py
@@ -16,30 +16,38 @@ if __name__ == '__main__':
 	x_path = dataset_path+"challenge/x_test.hdf5"
 	m_path = dataset_path+"challenge/x_test_metadata.csv"
 	y_path = dataset_path+"challenge/y_test.csv"
-	index=0
-	
-	x = read_x(x_path, index)
-	y = read_y(y_path, index)
-	metadata = pd.read_csv(m_path, index_col=0)
-	print(y.shape)
-	# exit()
+	y_pred = dataset_path+"challenge/y_benchmark.csv"
 
-	metadata = metadata.iloc[index].to_dict()
-	diameter = metadata['r']*2
-	
-	df, pred, label = dc.detect(x, diameter=diameter, 
-                                weights_path='output/weights/attention_unet_202206.pt', 
-                                patch_overlap=(16,16,16),
-                                debug=True, run_on="cpu",
-                                post_processing="log", batch_size=1)
-	
+	scores = []
+	for index in range(10):
+		
+		x = read_x(x_path, index)
+		y = read_y(y_path, index)
+		metadata = pd.read_csv(m_path, index_col=0)
+		print(y.shape)
 
-	
-	ap, precisions, recalls = average_precision(y, pred, diameter=diameter, canvas_size=x.shape)
-	fig = plot_pr(ap, precisions, recalls, name='Unet', tag='o-', color='red')
-	# ap, precisions, recalls, thresholds = average_precision(y, tp_pred, diameter=diameter)
-	# fig = plot_pr(ap, precisions, recalls, thresholds, name='trackpy', tag='x-', color='gray')
-	plt.show()
-	
-	dc.view(array=x, positions=pred, label=label)
-	
+		metadata = metadata.iloc[index].to_dict()
+		diameter = metadata['r']*2
+		print(metadata['r'])
+		
+		# df, pred, label = dc.detect(x, diameter=diameter, 
+		#                             weights_path='output/weights/attention_unet_202206.pt', 
+		#                             patch_overlap=(16,16,16),
+		#                             debug=True, run_on="cpu",
+		#                             post_processing="log", batch_size=1)
+		
+		pred = read_y(y_pred, index)
+
+		# pred, df = dc.run_trackpy(x, diameter=dc.round_up_to_odd(diameter))
+		
+		print(x.shape)
+		ap, precisions, recalls = average_precision(y, pred, diameter=metadata['r'], canvas_size=x.shape)
+		scores.append(ap)
+		# fig = plot_pr(ap, precisions, recalls, name='Unet', tag='o-', color='red')
+		# ap, precisions, recalls, thresholds = average_precision(y, tp_pred, diameter=diameter)
+		# fig = plot_pr(ap, precisions, recalls, thresholds, name='trackpy', tag='x-', color='gray')
+		# plt.show()
+		
+		# dc.view(array=x, positions=pred, label=label)
+		
+	print(scores)
