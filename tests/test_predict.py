175ab580369b2694d715850dc8e7170fd2f479ae
wahabk <akawafi3@gmail.com>
2023-05-29 15:55:44 +0100

    testing old model

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -8,17 +8,17 @@ from scipy import ndimage
 import tifffile
 
 dc = colloidoscope.DeepColloid()
-array = dc.read_tif('examples/Data/levke2.tiff')
+array = dc.read_tif('examples/Data/levke.tiff')
 print(array.shape, array.dtype, len(array.shape))
 # array = dc.read_tif('/home/wahab/Data/HDD/Colloids/Real/Levke/stedImagesLevke/smallOld_pxSize_good_large_30nm_5frames_decon.tif')[0]
 d = 15
 # array = ndimage.zoom(array, 1.5)
-weights_path = 'output/weights/attention_unet_202211.pt'
+weights_path = 'output/weights/attention_unet_202206.pt'
 array = dc.crop3d(array, roiSize=(200,500,500))
 # array = np.array([array, array])
 print(array.shape, array.dtype, len(array.shape))
-tifffile.imsave('examples/Data/levke.tiff', array)
-exit()
+
+
 df, positions, label = dc.detect(array, diameter=d, 
                                 weights_path=weights_path, 
                                 patch_overlap=(16,16,16),
@@ -26,6 +26,8 @@ df, positions, label = dc.detect(array, diameter=d,
                                 roiSize=(64,64,64), label_size=(60,60,60),
                                 post_processing="tp", batch_size=1)
 print(df)
+
+exit()
 array = np.array(array, dtype="uint8")
 tp_pred, df =  dc.run_trackpy(array, diameter=d)
 print(df)
